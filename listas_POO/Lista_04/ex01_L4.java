33ae39095fc9389f05fe8e780dea294b05dc2372
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-04-14 20:48:25 -0300

    ex01_feito

--- listas_POO/Lista_04/ex01_L4.java
+++ listas_POO/Lista_04/ex01_L4.java
@@ -1,5 +1,25 @@
-package listas_POO.Lista_04;
+import java.util.Scanner;
 
 public class ex01_L4 {
-
+    public static void main(String[] args) {
+        int n;
+        float media = 0;
+        float[] notas;
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Escreva a qnt de alunos: ");
+        n = kb.nextInt();
+        notas = new float[n];
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Escreva a nota do aluna %d: ", i + 1);
+            notas[i] = kb.nextFloat();
+            media += notas[i];
+        }
+        media /= n;
+        System.out.printf("Alunos acima da media (%.2f)\n", media);
+        for (int i = 0; i < n; i++) {
+            if (notas[i] >= media)
+                System.out.println(notas[i]);
+        }
+        kb.close();
+    }
 }
