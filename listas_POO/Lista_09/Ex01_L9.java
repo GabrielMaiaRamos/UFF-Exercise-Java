e01a9332ca8f938d65ba108daabfaebef517b298
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-06-20 13:41:38 -0300

    ex01_l9

---
+++ listas_POO/Lista_09/Ex01_L9.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Ex01_L9 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Digite uma frase:");
+        String frase = kb.nextLine();
+
+        String[] vetPalavras = frase.split(" "); // divide as palavras da frase num vetor
+
+        // converte o vetor estatico pra uma lista
+        List<String> listaPalavras = new ArrayList<>();
+        for (String palavra : vetPalavras) {
+            listaPalavras.add(palavra);
+        }
+        // ou pode converter com apenas essa funcao:
+        // List<String> listaPalavras = Arrays.asList(vetPalavras);
+
+        Collections.reverse(listaPalavras); // usa Colections para inverter a ordem da lista
+
+        // junta a lista em uma unica string unindo por com " " entre os itens
+        String fraseInvertida = String.join(" ", listaPalavras);
+
+        System.out.println("Frase invertida: " + fraseInvertida);
+        kb.close();
+    }
+}
