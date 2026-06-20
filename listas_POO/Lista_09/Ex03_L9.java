2bb54782db00d27436daa36eb95fcbe8ee1e8063
Gabriel Maia Ramos <ramosgabrielmaia@gmail.com>
2026-06-20 14:08:24 -0300

    ex03_l9

---
+++ listas_POO/Lista_09/Ex03_L9.java
@@ -0,0 +1,33 @@
+import java.util.Map;
+import java.lang.classfile.constantpool.IntegerEntry;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Ex03_L9 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Digite uma frase:");
+        String frase = kb.nextLine();
+
+        String[] vetPalavras = frase.split(" ");
+
+        // cria o hashmap ligando cada palavra(chave) à quantidade de ocorrências dela
+        Map<String, Integer> contagemMap = new HashMap<>();
+
+        // percorre todas as palavras para ir interando quandtas vezes aparece
+        for (String palavra : vetPalavras) {
+            // logica simples de: se ja existe, soma 1 no valor, se nao, adiciona como 1
+            if (contagemMap.containsKey(palavra)) {
+                int qtd = contagemMap.get(palavra);
+                contagemMap.put(palavra, qtd + 1);
+            } else
+                contagemMap.put(palavra, 1);
+        }
+
+        // printar o map
+        for (String palavra : contagemMap.keySet())
+            System.out.println(palavra + ": " + contagemMap.get(palavra));
+        kb.close();
+    }
+}
